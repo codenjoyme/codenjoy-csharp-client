addc254c618154c4f22c068cd179e687cedc664a
Dzmitry Lapunou <Dzmitry_Lapunou@epam.com>
2021-11-16 22:39:14 +0200

    clifford solver test

--- Dojo.Tests/Games/Clifford/SolverTests.cs
+++ Dojo.Tests/Games/Clifford/SolverTests.cs
@@ -5,5 +5,17 @@ namespace Dojo.Games.Clifford.Tests
     [Category("clifford")]
     public class SolverTests
     {
+        [Test]
+        public void Test_Answer()
+        {
+            var message = "board=" +
+                "☼☼☼☼☼" +
+                "☼   ☼" +
+                "☼ ⊳ ☼" +
+                "☼   ☼" +
+                "☼☼☼☼";
+            var solver = new CliffordSolver();
+            Assert.AreEqual("Act", solver.Get(new CliffordBoard(message)));
+        }
     }
 }
