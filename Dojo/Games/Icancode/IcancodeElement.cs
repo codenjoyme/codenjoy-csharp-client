34c23edefd58bb31ab55d925671c34a8636259ef
Oleksandr Baglai <oleksandr_baglai@epam.com>
2022-02-15 16:18:49 +0200

    [icancode][clients][all][elements-generator] Generate all clients for icancode.

--- Dojo/Games/Icancode/IcancodeElement.cs
+++ Dojo/Games/Icancode/IcancodeElement.cs
@@ -31,107 +31,211 @@ namespace Dojo.Games.Icancode
     public enum IcancodeElement : short
     {
 
+            // Empty space (on layer 2) where player can go.
+
         EMPTY = (short)'-',
+
+            // Empty space (on layer 1) where player can go.
 
         FLOOR = (short)'.',
 
+            // Wall (on layer 1).
+
         ANGLE_IN_LEFT = (short)'╔',
+
+            // Wall (on layer 1).
 
         WALL_FRONT = (short)'═',
 
+            // Wall (on layer 1).
+
         ANGLE_IN_RIGHT = (short)'┐',
+
+            // Wall (on layer 1).
 
         WALL_RIGHT = (short)'│',
 
+            // Wall (on layer 1).
+
         ANGLE_BACK_RIGHT = (short)'┘',
+
+            // Wall (on layer 1).
 
         WALL_BACK = (short)'─',
 
+            // Wall (on layer 1).
+
         ANGLE_BACK_LEFT = (short)'└',
+
+            // Wall (on layer 1).
 
         WALL_LEFT = (short)'║',
 
+            // Wall (on layer 1).
+
         WALL_BACK_ANGLE_LEFT = (short)'┌',
+
+            // Wall (on layer 1).
 
         WALL_BACK_ANGLE_RIGHT = (short)'╗',
 
+            // Wall (on layer 1).
+
         ANGLE_OUT_RIGHT = (short)'╝',
+
+            // Wall (on layer 1).
 
         ANGLE_OUT_LEFT = (short)'╚',
 
+            // Wall (on layer 1).
+
         SPACE = (short)' ',
+
+            // Charging laser machine (on layer 1) pointing left.
 
         LASER_MACHINE_CHARGING_LEFT = (short)'˂',
 
+            // Charging laser machine (on layer 1) pointing right.
+
         LASER_MACHINE_CHARGING_RIGHT = (short)'˃',
+
+            // Charging laser machine (on layer 1) pointing up.
 
         LASER_MACHINE_CHARGING_UP = (short)'˄',
 
+            // Charging laser machine (on layer 1) pointing down.
+
         LASER_MACHINE_CHARGING_DOWN = (short)'˅',
+
+            // Charged laser machine (on layer 1) pointing left.
 
         LASER_MACHINE_READY_LEFT = (short)'◄',
 
+            // Charged laser machine (on layer 1) pointing right.
+
         LASER_MACHINE_READY_RIGHT = (short)'►',
+
+            // Charged laser machine (on layer 1) pointing up.
 
         LASER_MACHINE_READY_UP = (short)'▲',
 
+            // Charged laser machine (on layer 1) pointing down.
+
         LASER_MACHINE_READY_DOWN = (short)'▼',
+
+            // Level start spot (on layer 1).
 
         START = (short)'S',
 
+            // Level exit spot (on layer 1).
+
         EXIT = (short)'E',
+
+            // Hole (on layer 1).
 
         HOLE = (short)'O',
 
+            // Box (on layer 2) that can be moved and jumped over.
+
         BOX = (short)'B',
+
+            // Zombie start spot (on layer 1).
 
         ZOMBIE_START = (short)'Z',
 
+            // Gold (on layer 1).
+
         GOLD = (short)'$',
+
+            // Unstoppable laser perk (on layer 1).
 
         UNSTOPPABLE_LASER_PERK = (short)'l',
 
+            // Death ray perk (on layer 1).
+
         DEATH_RAY_PERK = (short)'r',
+
+            // Unlimited fire perk (on layer 1).
 
         UNLIMITED_FIRE_PERK = (short)'f',
 
+            // Fire perk (on layer 1).
+
         FIRE_PERK = (short)'a',
+
+            // Jump perk (on layer 1).
 
         JUMP_PERK = (short)'j',
 
+            // Move boxes perk (on layer 1).
+
         MOVE_BOXES_PERK = (short)'m',
+
+            // Your hero (on layer 2).
 
         ROBO = (short)'☺',
 
+            // Your hero (on layer 2) falls into a hole.
+
         ROBO_FALLING = (short)'o',
+
+            // Your hero (on layer 3) falling.
 
         ROBO_FLYING = (short)'*',
 
+            // Your hero (on layer 2) died from a laser.
+
         ROBO_LASER = (short)'☻',
+
+            // Other hero (on layer 2).
 
         ROBO_OTHER = (short)'X',
 
+            // Other hero (on layer 2) falls into a hole.
+
         ROBO_OTHER_FALLING = (short)'x',
+
+            // Other hero (on layer 3) falling.
 
         ROBO_OTHER_FLYING = (short)'^',
 
+            // Other hero (on layer 2) died from a laser.
+
         ROBO_OTHER_LASER = (short)'&',
+
+            // Laser (on layer 2) flies to the left.
 
         LASER_LEFT = (short)'←',
 
+            // Laser (on layer 2) flies to the right.
+
         LASER_RIGHT = (short)'→',
+
+            // Laser (on layer 2) flies to the up.
 
         LASER_UP = (short)'↑',
 
+            // Laser (on layer 2) flies to the down.
+
         LASER_DOWN = (short)'↓',
+
+            // Female zombie (on layer 2).
 
         FEMALE_ZOMBIE = (short)'♀',
 
+            // Male zombie (on layer 2).
+
         MALE_ZOMBIE = (short)'♂',
+
+            // Zombie dies (on layer 2).
 
         ZOMBIE_DIE = (short)'✝',
 
+            // Fog of war system layer (on layer 1).
+
         FOG = (short)'F',
+
+            // Background system layer (on layer 2).
 
         BACKGROUND = (short)'G'
     }
